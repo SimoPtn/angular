3048961eafb103eda8bcec14d3a4d7666903e0e1
SimoPtn <ironist_92@hotmail.it>
2021-10-29 19:54:02 +0200

    add:exercise-angular-2

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User, Role, Gender } from '../models/user';
 
 @Component({
   selector: 'app-user-detail',
@@ -6,6 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
+
+  user: User = {
+    id: 3487,
+    name: 'Mario',
+    surname: 'Rossi',
+    age: 25,
+    dateOfBirth: '12-14-1995',
+    address: {
+        city: 'Roma',
+        street: 'Via roma 10',
+        postalCode: '00100'
+  },
+  role: Role.staff,
+    username: 'MarioRossi80',
+    profilePhotoUrl: 'https://bit.ly/3yRngEP',
+    companies: [
+		{
+            id: 148979,
+            name: 'Develhope',
+            description: 'La migliore',
+            location: {
+                city: 'Palermo',
+                street: 'Via Libertà 58',
+                postalCode: '90139'
+            }
+        },
+        {
+            id: 123123,
+            name: 'Apple',
+            description: 'E insomma...',
+            location: {
+                city: 'Cupertino',
+                street: 'One Apple Park Way',
+                postalCode: '95014'
+            }
+        }
+	],
+    gender: Gender.male
+}
 
   constructor() { }
 
