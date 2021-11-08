abaf487388a975dea25ebfc5130ca34b0a57c3d6
SimoPtn <86603001+SimoPtn@users.noreply.github.com>
2021-11-08 12:55:20 +0100

    Update user-list.component.ts

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -6,7 +6,7 @@ import { Obj, Role, Gender } from '../models/obj';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
-  obj: Objs[] = [
+  objsUser: Obj[] = [
     {
       id: 3487,
       name: 'Mario',
